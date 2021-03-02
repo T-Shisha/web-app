ff0ad86a4e6684de79298c64114c5a38fea2cc86
User <tanya.shisha2001@gmail.com>
2021-03-02 18:22:18 +0300

    create logic

---
+++ logic.js
@@ -0,0 +1,8 @@
+function addOne() {
+
+    document.form.number.value = Number(document.form.number.value) + 1;
+}
+
+function subtractOne() {
+    document.form.number.value = Number(document.form.number.value) - 1;
+}
